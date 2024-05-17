f8b2e01dfd0f9629ebd00721f75b4908ac8740c2
zillurahman10 <zillur.hero40@gmail.com>
2024-05-17 19:57:33 +0600

    problem_1 solved

---
+++ problem_1.js
@@ -0,0 +1,23 @@
+var result = 80;
+
+if(result < 33){
+    console.log("F")
+}
+else if(result > 33  && result <= 40 ){
+    console.log("D")
+}
+else if(result > 40  && result <= 50 ){
+    console.log("C")
+}
+else if(result > 50  && result <= 60 ){
+    console.log("B")
+}
+else if(result > 60  && result <= 70 ){
+    console.log("A")
+}
+else if(result > 70  && result <= 80 ){
+    console.log("A")
+}
+else if(result > 80  && result <= 90 ){
+    console.log("A+")
+}
